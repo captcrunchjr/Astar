27d7bb8a27a3b56bfcd3f05cfc123f620bc971e1
captcrunchjr <82350352+captcrunchjr@users.noreply.github.com>
2023-07-22 09:13:50 -0400

    .

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EightQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
